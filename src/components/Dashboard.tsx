96393bd8ccfe618ffb4c9dbfe537ec8c269b260c
Joey Kwasniewski <kwasniewski@gmail.com>
2025-05-23 15:02:07 -0700

    feat: use nicely formatted code blocks

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useStytch, useStytchSession, useStytchUser } from '@stytch/react';
 import { Navigate } from 'react-router';
+import { CopyBlock } from 'react-code-blocks';
 
 const Dashboard: React.FC = () => {
   const stytch = useStytch();
@@ -13,25 +14,32 @@ const Dashboard: React.FC = () => {
 
   return (
     <div className="dashboard-container">
-      <h1>Dashboard</h1>
-      <h1>Profile</h1>
+      <div className="dashboard-header">
+        <h1>Profile</h1>
+        <button className="primary" onClick={() => stytch.session.revoke()}>
+          Log out
+        </button>
+      </div>
       <h2>User object</h2>
-      <pre className="code-block">
-        <code>{JSON.stringify(user, null, 2)}</code>
-      </pre>
+      <CopyBlock
+        text={JSON.stringify(user, null, 2)}
+        language="json"
+        showLineNumbers={true}
+        wrapLongLines={true}
+      />
 
       <h2>Session object</h2>
-      <pre className="code-block">
-        <code>{JSON.stringify(session, null, 2)}</code>
-      </pre>
+      <CopyBlock
+        text={JSON.stringify(session, null, 2)}
+        language="json"
+        showLineNumbers={true}
+        wrapLongLines={true}
+      />
       <p>
         You are logged in, and a Session has been created. The SDK stores the Session as a token and
         a JWT in the browser cookies as <span className="code">stytch_session</span> and{' '}
         <span className="code">stytch_session_jwt</span> respectively.
       </p>
-      <button className="primary" onClick={() => stytch.session.revoke()}>
-        Log out
-      </button>
     </div>
   );
 };
